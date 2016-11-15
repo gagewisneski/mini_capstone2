0dae4bae3e8d21aac3c4a236c5d7661535c4832e
gagewisneski <gagewisneski@gmail.com>
2016-11-14 20:35:11 -0800

    Added show page and more css

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,20 @@ class ProductsController < ApplicationController
 
   end
 
+  def new_product
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  def create_product
+    @product = Product.new(name: params[:name], price: params[:price].to_i, image: params[:image], description: params[:description])
+    @product.save
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   get "/", to: "products#home"
 
+  get "/new_product", to: "products#new_product"
+  post "/create_product", to: "products#create_product"
+
+  get "/products", to: "products#index"
+
+
+  #make sure routes aren't overriden
+  get "/products/:id", to: "products#show"
+
 end
