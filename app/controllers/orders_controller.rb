ee1c57f3c5d16e72853f2fef7fe0b0a8bf31a4f8
gagewisneski <gagewisneski@gmail.com>
2016-12-01 20:48:21 -0800

    No longer using cookies to create order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
     user = User.find_by(id: current_user.id)
     @cartedproducts = user.carted_products.where("status LIKE ?", "carted")
 
-    @order = Order.new(user_id: current_user.id, subtotal: session[:subtotal], tax: session[:tax], total: session[:total])
+    @order = Order.new(user_id: current_user.id, subtotal: params[:subtotal].to_i, tax: params[:tax].to_i, total: params[:total].to_i)
     @order.save
 
     @cartedproducts.each do |product|
